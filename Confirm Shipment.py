c73d8fa25dd0fe35cec3897efaebb76764b8ded0
lmichaele <lmichaele@gmail.com>
2017-02-01 16:19:10 +1100

    Add files via upload

--- Confirm Shipment.py
+++ Confirm Shipment.py
@@ -1,4 +1,6 @@
 import openpyxl, csv, os, time, glob, shutil, datetime
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
 
 print ('Before starting, make sure all csv invoices are saved in Supply Chain > CSV > New Shipment.')
 
@@ -6,7 +8,7 @@ EDR = openpyxl.load_workbook('G:\\Supply Chain\\Data\\SHIPMENTS\\Electronic Regi
 
 sheet = EDR.get_sheet_by_name('Register')
 
-sheet.max_row
+#sheet.max_row
 
 lship = sheet.cell(row=(sheet.max_row), column=2).value
 
@@ -39,8 +41,10 @@ with open("invoice.csv", "wb") as outfile:
     for f in read_files:
         with open(f, "rb") as infile:
             outfile.write(infile.read())
+
+master_invoice = (directory)+"SHIP"+str(SHIP)+"_master_invoice.csv" 
 			
-shutil.move("invoice.csv",(directory)+"SHIP"+str(SHIP)+".csv")
+shutil.move("invoice.csv",master_invoice)
 
 time.sleep(2)
 
@@ -147,7 +151,121 @@ while True:
     else:
         print("Not a valid option. Please try again")
 
-#TODO add to ER
+print("Now I am going to update the Electronic Register...")
 
+directory = "G:\\Supply Chain\\Ship Files\\SHIP" + str(SHIP) + "\\"
+
+os.chdir(directory)
+
+f = open(file)
+         
+exampleReader = csv.reader(f)
+data = list(exampleReader)
+invoices = []
+r=0
+for row in data:
+    if data[r][0] == 'IDH':
+        invoices.append(data[r][5])
+    r=r+1
+
+FCV = []
+r=0
+for row in data:
+    if data[r][0] == 'IDH':
+        FCV.append(data[r][9])
+    r=r+1
+
+CI = []
+r=0
+for row in data:
+    if data[r][0] == 'IDH':
+        CIs = (float(data[r][9]))-(float(data[r][7]))
+        CI.append(CIs)
+    r=r+1
+
+WH = []
+r=0
+for row in data:
+    if data[r][1] == '8682':
+        WH.append(1)
+    elif data[r][1] == '8686':
+        WH.append(2)
+    elif data[r][1] == '8728':
+        WH.append(4)
+    elif data[r][1] == '8687':
+        WH.append(5)
+    elif data[r][1] == '18682':
+        WH.append(1)
+    elif data[r][1] == '18686':
+        WH.append(2)
+    elif data[r][1] == '18728':
+        WH.append(4)
+    elif data[r][1] == '18687':
+        WH.append(5)
+    elif data[r][1] == '8726':
+        WH.append(1)
+    elif data[r][1] == '8986':
+        WH.append(2)
+    elif data[r][1] == '8928':
+        WH.append(4)
+    elif data[r][1] == '8688':
+        WH.append(5)
+    elif data[r][1] == '18726':
+        WH.append(1)
+    elif data[r][1] == '18986':
+        WH.append(2)
+    elif data[r][1] == '18928':
+        WH.append(4)
+    elif data[r][1] == '18688':
+        WH.append(5)
+    r=r+1
         
+Supplier = []
+r=0
+for row in data:
+    if data[r][1] == '8682':
+        Supplier.append(1)
+    elif data[r][1] == '8686':
+        Supplier.append(1)
+    elif data[r][1] == '8728':
+        Supplier.append(1)
+    elif data[r][1] == '8687':
+        Supplier.append(1)
+    elif data[r][1] == '18682':
+        Supplier.append(24)
+    elif data[r][1] == '18686':
+        Supplier.append(24)
+    elif data[r][1] == '18728':
+        Supplier.append(24)
+    elif data[r][1] == '18687':
+        Supplier.append(24)
+    elif data[r][1] == '8726':
+        Supplier.append(732)
+    elif data[r][1] == '8986':
+        Supplier.append(732)
+    elif data[r][1] == '8928':
+        Supplier.append(732)
+    elif data[r][1] == '8688':
+        Supplier.append(732)
+    elif data[r][1] == '18726':
+        Supplier.append(735)
+    elif data[r][1] == '18986':
+        Supplier.append(735)
+    elif data[r][1] == '18928':
+        Supplier.append(735)
+    elif data[r][1] == '18688':
+        Supplier.append(735)
+    r=r+1
+
+GBP = c.get_rate('GBP','AUD')
+AUD = []
+for i in FCV:
+   AUD.append(float(i) * GBP)
+
+XR = []
+for i in AUD:
+    XR.append(GBP)
+
+
+
 
---
+++ demo.py
@@ -0,0 +1,149 @@
+import openpyxl, csv, os, datetime
+from forex_python.converter import CurrencyRates
+c = CurrencyRates()
+
+directory = "G:\\Supply Chain\\Ship Files\\SHIP1175\\"
+
+os.chdir(directory)
+
+SHIP = 1175
+
+f = open("SHIP1175.csv")
+exampleReader = csv.reader(f)
+data = list(exampleReader)
+invoices = []
+r=0
+for row in data:
+    if data[r][0] == 'IDH':
+        invoices.append(data[r][5])
+    r=r+1
+
+FCV = []
+r=0
+for row in data:
+    if data[r][0] == 'IDH':
+        FCV.append(data[r][9])
+    r=r+1
+
+CI = []
+r=0
+for row in data:
+    if data[r][0] == 'IDH':
+        CIs = (float(data[r][9]))-(float(data[r][7]))
+        CI.append(CIs)
+    r=r+1
+
+WH = []
+r=0
+for row in data:
+    if data[r][1] == '8682':
+        WH.append(1)
+    elif data[r][1] == '8686':
+        WH.append(2)
+    elif data[r][1] == '8728':
+        WH.append(4)
+    elif data[r][1] == '8687':
+        WH.append(5)
+    elif data[r][1] == '18682':
+        WH.append(1)
+    elif data[r][1] == '18686':
+        WH.append(2)
+    elif data[r][1] == '18728':
+        WH.append(4)
+    elif data[r][1] == '18687':
+        WH.append(5)
+    elif data[r][1] == '8726':
+        WH.append(1)
+    elif data[r][1] == '8986':
+        WH.append(2)
+    elif data[r][1] == '8928':
+        WH.append(4)
+    elif data[r][1] == '8688':
+        WH.append(5)
+    elif data[r][1] == '18726':
+        WH.append(1)
+    elif data[r][1] == '18986':
+        WH.append(2)
+    elif data[r][1] == '18928':
+        WH.append(4)
+    elif data[r][1] == '18688':
+        WH.append(5)
+    r=r+1
+        
+Supplier = []
+r=0
+for row in data:
+    if data[r][1] == '8682':
+        Supplier.append(1)
+    elif data[r][1] == '8686':
+        Supplier.append(1)
+    elif data[r][1] == '8728':
+        Supplier.append(1)
+    elif data[r][1] == '8687':
+        Supplier.append(1)
+    elif data[r][1] == '18682':
+        Supplier.append(24)
+    elif data[r][1] == '18686':
+        Supplier.append(24)
+    elif data[r][1] == '18728':
+        Supplier.append(24)
+    elif data[r][1] == '18687':
+        Supplier.append(24)
+    elif data[r][1] == '8726':
+        Supplier.append(732)
+    elif data[r][1] == '8986':
+        Supplier.append(732)
+    elif data[r][1] == '8928':
+        Supplier.append(732)
+    elif data[r][1] == '8688':
+        Supplier.append(732)
+    elif data[r][1] == '18726':
+        Supplier.append(735)
+    elif data[r][1] == '18986':
+        Supplier.append(735)
+    elif data[r][1] == '18928':
+        Supplier.append(735)
+    elif data[r][1] == '18688':
+        Supplier.append(735)
+    r=r+1
+
+GBP = c.get_rate('GBP','AUD')
+AUD = []
+for i in FCV:
+   AUD.append(float(i) * GBP)
+
+XR = []
+for i in AUD:
+    XR.append(GBP)
+
+SHIPLIST = []
+for i in invoices:
+    SHIPLIST.append(SHIP)
+
+EDR = openpyxl.load_workbook('G:\\Supply Chain\\Data\\SHIPMENTS\\Electronic Register - SGS - Copy.xlsx')
+sheet = EDR.get_sheet_by_name('Register')
+
+nextrow = int(sheet.max_row)+1
+
+r=0
+while r < len(invoices):
+    for i in SHIPLIST:
+        c = EDR['Register']['B'+str(nextrow)]
+        c.value = int(i)
+    for i in invoices:
+        c = EDR['Register']['K'+str(nextrow)]
+        c.value = int(i)
+    for i in WH:
+        c = EDR['Register']['G'+str(nextrow)]
+        c.value = int(i)
+
+        r=r+1
+        nextrow = nextrow + 1
+        
+EDR.save('G:\\Supply Chain\\Data\\SHIPMENTS\\Electronic Register - SGS - Copy.xlsx')
+
+
+
+
+
+
