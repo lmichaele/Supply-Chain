891eff67daaf34118f0ddb698e60412a2e689cee
Edwards <luke.edwards@agcocorp.com>
2020-09-14 15:00:02 +1000

    updating

--- pdfreader.py
+++
@@ -1,4 +0,0 @@
-import PyPDF2
-pdfFileObj = open('SVR1.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-pdfReader.numPages
--- xl.py
+++
@@ -1,7 +0,0 @@
-import openpyxl
-wb = openpyxl.load_workbook('example.xlsx')
-sheet = wb.active
-sheet.columns[1] (<Cell Sheet1.B1>, <Cell Sheet1.B2>, <Cell Sheet1.B3>, <Cell Sheet1.B4>,
-<Cell Sheet1.B5>, <Cell Sheet1.B6>, <Cell Sheet1.B7>)
->>> for cellObj in sheet.columns[1]:
-        print(cellObj.value)
